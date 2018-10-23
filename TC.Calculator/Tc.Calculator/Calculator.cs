64c09068bae0da2033ed162c85ae53199c46a0fc
qdimka <qdimka1991@mail.ru>
2018-10-23 15:23:58 +0300

    Добавлены файлы проекта

---
+++ TC.Calculator/Tc.Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tc
+{
+	public class Calculator
+	{
+		public int Sum(int first, int second)
+			=> first + second;
+
+		public int Division(int first, int second)
+		{
+			if(second == 0)
+				throw new InvalidOperationException(nameof(second));
+
+			return first / second;
+		}
+	}
+}
---
+++ TC.Calculator/Tc.CalculatorTest/CalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tc.CalculatorTest
+{
+	public class CalculatorTests
+	{
+		private readonly Calculator _calculator;
+
+		public CalculatorTests()
+		{
+			_calculator = new Calculator();
+		}
+
+		[Fact(DisplayName = "Метод возвращает сумму двух элементов")]
+		public void SumReturnSumOfTwoIntegersTest()
+		{
+			var first = 1;
+			var second = 3;
+			var expected = 4;
+
+			var actual = _calculator.Sum(first, second);
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact(DisplayName = "Метод бросает исключение InvalidOperationException при попытке деления на ноль")]
+		public void DivisionThrowsInvalidOperationExceptionForDivisionByZero()
+		{
+			var first = 1;
+			var second = 0;
+
+			Assert.Throws<InvalidOperationException>(() => _calculator.Division(first, second));
+		}
+	}
+}
